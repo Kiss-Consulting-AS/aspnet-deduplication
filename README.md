8ad43cadb458a3db3e3100add4e1050bf075fbdd
Morten <morten@kiss.no>
2024-02-08 11:07:00 +0100

    Add Dockerignore, gitignore files and project setup
    A new .dockerignore, gitignore files, and project files have been added to improve the project setup. The commit primarily includes essential files for developing in a Docker environment and supports .NET 8.0. The commit also adds a few code files, such as Program.cs and Dockerfile for Kiss.AspNet.Deduplication.TestApplication, setup for running in AWS environment, and other Jetbrain IDE settings.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# aspnet-deduplication
+# Kiss Asp.Net Deduplication
+![Build status](https://ci.appveyor.com/api/projects/status/7)   
+![NuGet](https://img.shields.io/nuget/v/KissAsp.Net.Deduplication)  
+![License](https://img.shields.io/github/license/andrew-gresyk/KissAsp.Net.Deduplication)  
+![GitHub issues](https://img.shields.io/github/issues/andrew-gresyk/KissAsp.Net.Deduplication)  
+![GitHub pull requests](https://img.shields.io/github/issues-pr/andrew-gresyk/KissAsp.Net.Deduplication)  
+![GitHub last commit](https://img.shields.io/github/last-commit/andrew-gresyk/KissAsp.Net.Deduplication)  
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/andrew-gresyk/KissAsp.Net.Deduplication)  
+![GitHub contributors](https://img.shields.io/github/contributors/andrew-gresyk/KissAsp.Net.Deduplication)  
+![GitHub top language](https://img.shields.io/github/languages/top/andrew-gresyk/KissAsp.Net.Deduplication)  
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/andrew-gresyk/KissAsp.Net.Deduplication)  
+  
 A small library with a single purpose: enable idempotent-like behavior in POST requests.
---
+++ src/Kiss.AspNet.Deduplication/Builders/DeduplicationBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+namespace Kiss.AspNet.Deduplication;
+
+public interface IDeduplicationBuilder
+{
+    /// <summary>
+    /// Gets a value indicating whether the SupportsAot feature is supported.
+    /// </summary>
+    /// <remarks>
+    /// The SupportsAot property determines whether the deduplication setup supports Ahead-of-Time (AOT) compilation.
+    /// AOT compilation is a technique where an application is compiled into executable code before it is run, rather than compiling it during runtime like Just-in-Time (JIT) compilation
+    /// .
+    /// If the SupportsAot property returns true, it means that the system supports AOT compilation. Otherwise, if it returns false, it means that the system does not support AOT compilation
+    /// .
+    /// The builder will use the SupportsAot property to determine whether to use AOT compilation or not
+    /// </remarks>
+    bool SupportsAot { get; }
+
+    void Build();
+}
+
+/// <inheritdoc cref="IDeduplicationBuilder"/>
+public class DeduplicationBuilder
+    : IDeduplicationBuilder
+{
+    public bool SupportsAot => false;
+    
+    public void Build()
+    {
+        throwIfReflectionBasedSerializationDisabled();
+    }
+
+    private static void throwIfReflectionBasedSerializationDisabled()
+    {
+        if (!JsonSerializer.IsReflectionEnabledByDefault)
+            throw new NotSupportedException("""
+                                            The deduplication feature requires reflection-based serialization 
+                                            to be enabled. Please enable reflection-based serialization by 
+                                            setting the JsonSerializerIsReflectionEnabledByDefault setting in 
+                                            your project file.
+                                            https://devblogs.microsoft.com/dotnet/system-text-json-in-dotnet-8/
+                                            """);
+    }
+}
